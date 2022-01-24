6ccb26824a46aebc418f8aa0d55d8550742acecd
polinaprinii <x21137757@student.ncirl.ie>
2022-01-24 22:54:45 +0000

    Update Week 1 Lab.py

--- Data Mining and Machine Learning 1/Week 1 Lab.py
+++ Data Mining and Machine Learning 1/Week 1 Lab.py
@@ -11,3 +11,34 @@ if (num % 2) == 0:
     print("{0} is an Even number".format(num))
 else:
     print("{0} is an Odd number".format(num))
+
+"""
+Exercise 2
+Ask the user for a string and print out whether this string is a palindrome or not. A palindrome is a string
+that reads the same forwards and backwards.
+"""
+
+# Input for a string.
+text = input("Please enter a word here, the machine will tell if the word is a palindrome or not.")
+reverse = reversed(text)
+
+# If statement to determine if word is a palindrome word.
+if list(text) == list(reverse):
+    print("The provided is a palindrome word.")
+else:
+    print("The provided is not a palindrome word.")
+
+"""
+Exercise 3
+Write a program that takes a list of numbers (for example, a = [10, 25, 9, 23]) and 
+create a new list with the first and last elements of the given list.
+"""
+# Creating a list.
+mylist = [1, 2, 3, 4, 5]
+print(mylist)
+
+# Using indexes we create a new list with the first and last element of the previous list.
+newlist = [mylist[0], mylist[-1]]
+print(newlist)
+
+
