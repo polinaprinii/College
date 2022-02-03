ca3504b21612684b0d1f0cfb16aaa03c6c032a1e
polinaprinii <x21137757@student.ncirl.ie>
2022-02-03 21:11:58 +0000

    Update Week 2 Lab.py

--- Data Mining and Machine Learning 1/Week 2 Lab.py
+++ Data Mining and Machine Learning 1/Week 2 Lab.py
@@ -6,9 +6,14 @@ Read its content. Write frequency of the words of the text in a file.
 Use python’s dictionary data structure for storing frequencies of the words. 
 [hint: consider word as key and frequency of that word as value]
 """
-
+# Importing text file into PyCharm
 textfile = open("/Users/polinaprinii/Downloads/Lab - Python Exercise Tokeniser Problem.en-fr.en", "r")
+# Printing our variable for verification purposes.
 print(textfile.read())
+
+# Setting our text file as a variable:
+myfile = textfile.read()
+
 
 
 
