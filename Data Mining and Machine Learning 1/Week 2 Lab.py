2ea135464b237c71d66b6c72512b4de569512487
polinaprinii <x21137757@student.ncirl.ie>
2022-02-03 21:09:38 +0000

    Create Week 2 Lab.py

---
+++ Data Mining and Machine Learning 1/Week 2 Lab.py
@@ -0,0 +1,15 @@
+# Tokeniser Problem
+
+"""
+Take a text file (uploaded to Moodle) as an input of your program. 
+Read its content. Write frequency of the words of the text in a file. 
+Use python’s dictionary data structure for storing frequencies of the words. 
+[hint: consider word as key and frequency of that word as value]
+"""
+
+textfile = open("/Users/polinaprinii/Downloads/Lab - Python Exercise Tokeniser Problem.en-fr.en", "r")
+print(textfile.read())
+
+
+
+
