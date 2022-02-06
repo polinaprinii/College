19675072d253b9f9d60adfb3363de7b155476637
polinaprinii <x21137757@student.ncirl.ie>
2022-02-06 21:06:31 +0000

    Create Week 3 Lab.py

---
+++ Data Mining and Machine Learning 1/Week 3 Lab.py
@@ -0,0 +1,15 @@
+"""
+The objective of the exercises is to familiarize yourself with the use of NumPy.
+These excises are mainly based on rainfall data in Cork for each month over the past half century.
+In the folder you will find a file called CorkRainfall.txt and a file called DublinRainfall.txt.
+This is a space delimited file.
+Each line of the file contains the following precipitation information pertaining to a specific month and year:
+Year
+Month (1 = Jan, 2 = Feb, 3 = March, etc. )  Total Rainfall (Millimetres)
+Most Rainfall in a Day (Millimetres)
+Rain days (0.2mm or More) (Number)
+
+Please use NumPy to answer the following questions.
+The objective of this task is to familiarize yourself with the operation of NumPy.
+(There is no need to incorporate error checking).
+"""
