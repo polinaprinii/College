f64a1d62fd3107b35c65c7e817e4b159e49b87bd
polinaprinii <x21137757@student.ncirl.ie>
2022-02-09 20:58:29 +0000

    Update Week 4 Lab.py

--- Data Mining and Machine Learning 1/Week 4 Lab.py
+++ Data Mining and Machine Learning 1/Week 4 Lab.py
@@ -23,19 +23,19 @@ df = pd.read_csv('attacks.csv', encoding = "ISO-8859-1")
 """
 
 import pandas as pd
-# """
-# (i)
-# What location globally has the highest number of shark attacks?
-# """
-# # Importing file to pandas.
-# df = pd.read_csv('/Users/polinaprinii/Downloads/Lab -- attacks.csv', encoding = "ISO-8859-1")
-# print(df)
-#
-# # Checking data types of each column.
-# print(df.dtypes)
-#
-# # Grouping data by Country to determine the location.
-#
-# df_group = df.groupby('Country')['Case Number'].nlargest(6)
-#
-# print(df_group)
+"""
+(i)
+What location globally has the highest number of shark attacks?
+"""
+# Importing file to pandas.
+df = pd.read_csv('/Users/polinaprinii/Downloads/Lab -- attacks.csv', encoding = "ISO-8859-1")
+print(df)
+
+# Checking data types of each column.
+print(df.dtypes)
+
+# Grouping data by Country to determine the location.
+
+df_group = df.groupby('Country')['Case Number'].nlargest(6)
+
+print(df_group)
