292da8aed6ea1184227c6d73da0e7e45de1dfa9b
polinaprinii <x21137757@student.ncirl.ie>
2022-03-15 16:16:04 +0000

    Update Week 5.py

--- Data Mining and Machine Learning 1/Week 5.py
+++ Data Mining and Machine Learning 1/Week 5.py
@@ -11,6 +11,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
 import seaborn as sns
+from scipy.stats import zscore
+from scipy import stats
+import numpy as np
 
 # Next we define our datasets
 train = pd.read_csv('/Users/polinaprinii/Downloads/Lab -- Outliers Detection/trainOutlier.csv')
@@ -41,3 +44,24 @@ print("The Decision Tree Regression Accuracy is: ", round(reg.score(test_feature
 # Now we move to identify outliers for both train and test.
 sns.boxplot(data=pd.DataFrame(train_features))
 plt.show()
+
+sns.boxplot(data=pd.DataFrame(test_features))
+plt.show()
+
+# Now we move to remove the identified outliers, we do so using z scores.
+#Removing outliers for train dataset
+z_scores_train = stats.zscore(train)
+
+abs_z_scores_train = np.abs(z_scores_train)
+filtered_entries_train = (abs_z_scores_train < 3).all(axis=1)
+new_df_train = train[filtered_entries_train]
+
+# Removing outliers for test dataset
+z_scores_test = stats.zscore(test)
+
+abs_z_scores_test = np.abs(z_scores_test)
+filtered_entries_test = (abs_z_scores_test < 3).all(axis=1)
+new_df_test = test[filtered_entries_train]
+
+# Now we set the features and labels to the new df's after removing the outliers.
+# For
