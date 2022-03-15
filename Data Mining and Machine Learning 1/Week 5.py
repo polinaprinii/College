15c120087c82c4ead1d0461527a975dda278ded5
polinaprinii <x21137757@student.ncirl.ie>
2022-03-15 00:17:14 +0000

    Update Week 5.py

--- Data Mining and Machine Learning 1/Week 5.py
+++ Data Mining and Machine Learning 1/Week 5.py
@@ -10,11 +10,31 @@ Read this data into your program.
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
 
 # Next we define our datasets
 train = pd.read_csv('/Users/polinaprinii/Downloads/Lab -- Outliers Detection/trainOutlier.csv')
 test = pd.read_csv('/Users/polinaprinii/Downloads/Lab -- Outliers Detection/test.csv')
 
-# Sanity check to see the data is correctly importing.
-print(train.head, "\n")
-print(test.head, "\n")
+# See the structure of each dataset
+train.shape
+test.shape
+
+# See the first 5 rows
+train.head()
+test.head()
+
+# We now split both the train and test data into features and labels.
+train_features = train.iloc[:, :-1].values
+train_labels = train.iloc[:, -1].values
+# We must add .values to all features and labels to avoid FutureWarnings as we are working with data with no headings.
+test_features = test.iloc[:, :-1].values
+test_labels = test.iloc[:, -1].values
+
+# Now we apply a Decision Tree Regression to assess accuracy.
+reg = DecisionTreeRegressor()
+reg = reg.fit(train_features, train_labels)
+print("The Decision Tree Regression Accuracy is: ", round(reg.score(test_features, test_labels), 2),
+      "which as a percentage is: ", round(reg.score(test_features, test_labels), 2) * 100, "%", "\n"
+      "Please note we have not yet addressed any outliers.", "\n")
+
