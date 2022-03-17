8cc44b2ed963fc8798f29e4d6fda494dec072ddd
polinaprinii <x21137757@student.ncirl.ie>
2022-03-17 17:55:09 +0000

    Update Week 7 Lab.py
    a) Decision Tree Classifier model.

--- Data Mining and Machine Learning 1/Week 7 Lab.py
+++ Data Mining and Machine Learning 1/Week 7 Lab.py
@@ -11,10 +11,38 @@ e.	K-NN with different values of K
 # The dataset will be derived from the mlxtend library.
 
 # Importing all necessary libraries:
-import numpy as np
+from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import BaggingClassifier
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import BaggingRegressor
 from sklearn.neighbors import KNeighborsRegressor
+from mlxtend.data import iris_data
+from mlxtend.evaluate import bias_variance_decomp
 
+
+# Importing data:
+data = iris_data()
+
+# Seperate feautures and labels:
+X, y = data
+
+# Split data into train and test data:
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+# a) Decision Tree Classifier:
+# Define the model:
+modelA = DecisionTreeClassifier()
+
+# Calculate the average expected loss, bias and variance.
+avg_expected_loss, avg_bias, avg_var = bias_variance_decomp(
+        modelA, X_train, y_train, X_test, y_test,
+        loss='0-1_loss',
+        random_seed=123)
+
+# Print results:
+print('Average expected loss: %.3f' % avg_expected_loss)
+print('Average bias: %.3f' % avg_bias)
+print('Average variance: %.3f' % avg_var)
+
+# b) Ensemble (Bagging classifier)
